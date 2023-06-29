00f4d29c17b78868d2a9bf192eef9535cf8aed88
Euccreative <euccreative@gmail.com>
2023-06-29 14:24:36 +0100

    Created a module for additon, subtraction, division and multiplication

---
+++ calculator.py
@@ -0,0 +1,35 @@
+#!/c/Users/User/AppData/Local/Microsoft/WindowsApps/python3
+
+
+def add(*args):
+    """To add the inputs given by the user"""
+    value = 0
+    for num in args:
+        value += num
+    return value
+
+def sub(*args):
+    """To subtract the inputs given by the user"""
+    value = args[0]
+    for num in args[1:]:
+        value -= num
+    return value
+
+def div(*args):
+    """To divide the inputs given by the user"""
+    value = args[0] #assign the numerator which is the first arguments to value
+    for num in args[1:]: #start for the index 1 to loop through the arguments
+        value /= num
+    return value
+
+def mul(*args):
+    """To multipy the inputs given by the users"""
+    value = 1 #anything multiply by 1 is 1 
+    for num in args:
+        value *= num
+    return value
+
+#print(add(4, 5))
+#print(sub(2, 5))
+#print(div(20, 10, 30))
+#print(mul(2.0, 2))
